909a30911152e55d90e2ea853dde79327e845217
AaravG42 <anujaarav06@gmail.com>
2025-08-18 22:05:30 +0530

    logo

--- src/components/MEALogo.tsx
+++ src/components/MEALogo.tsx
@@ -5,17 +5,10 @@ interface MEALogoProps {
   className?: string;
 }
 
-const MEALogo: React.FC<MEALogoProps> = ({ className = "h-12 w-12" }) => {
+const MEALogo: React.FC<MEALogoProps> = ({ className = "h-16 w-16" }) => {
   return (
     <div className={`relative ${className}`}>
-      <div className="absolute inset-0 flex items-center justify-center">
-        <div className="animate-gear-spin h-full w-full text-mea-gold">
-          <svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-            <path d="M12 15.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
-            <path d="M19.43 12.98c.04-.32.07-.64.07-.98 0-.34-.03-.66-.07-.98l2.11-1.65c.19-.15.24-.42.12-.64l-2-3.46c-.12-.22-.39-.3-.61-.22l-2.49 1c-.52-.4-1.08-.73-1.69-.98l-.38-2.65C14.46 2.18 14.25 2 14 2h-4c-.25 0-.46.18-.49.42l-.38 2.65c-.61.25-1.17.59-1.69.98l-2.49-1c-.23-.09-.49 0-.61.22l-2 3.46c-.13.22-.07.49.12.64l2.11 1.65c-.04.32-.07.65-.07.98 0 .33.03.66.07.98l-2.11 1.65c-.19.15-.24.42-.12.64l2 3.46c.12.22.39.3.61.22l2.49-1c.52.4 1.08.73 1.69.98l.38 2.65c.03.24.24.42.49.42h4c.25 0 .46-.18.49-.42l.38-2.65c.61-.25 1.17-.59 1.69-.98l2.49 1c.23.09.49 0 .61-.22l2-3.46c.12-.22.07-.49-.12-.64l-2.11-1.65z"/>
-          </svg>
-        </div>
-      </div>
+      <img src="/logoanim.svg" alt="MEA Logo" className="h-full w-full" />
     </div>
   );
 };
